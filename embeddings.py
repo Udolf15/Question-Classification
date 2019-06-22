bcd992ba085abee038aeb7338616e6605ed63fbe
Udolf15 <udolf15@gmail.com>
2019-06-22 19:22:11 +0530

    updates on embedding.py  with print statements

--- embeddings.py
+++ embeddings.py
@@ -9,11 +9,13 @@ class Embeddings:
 
     def create_embeddings(self):
 
+        print("Loading Embeddings .....................")
         en_model = KeyedVectors.load_word2vec_format('data/cc.hi.300.vec')
-
+        print("Embeddings Loaded")
         emb = {}
 
         for word in en_model.vocab:
             emb[word] = en_model[word]
 
+        print("Embeddings dictionary Created -s")
         return emb
