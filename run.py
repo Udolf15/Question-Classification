21948d697fb79527324c00ed16c5d320ada2278f
Udolf15 <udolf15@gmail.com>
2019-06-22 20:26:43 +0530

    Add presentation and comments in run.py

--- run.py
+++ run.py
@@ -2,29 +2,39 @@ from modelCreation import *
 from embeddings import *
 from preprocessing import *
 
+# Init the modules created
 preProcessor = Preprocessing()
-batch_size = 32
-SEQ_LEN = 80
-
 modelObject = ModelC()
 embeddings = Embeddings()
 
-train_df = preProcessor.read_csv('data/questions.csv')
-train_df = preProcessor.dfPreprocess(train_df)
-
-emb = embeddings.create_embeddings()
-
+# Constants 
+batch_size = 32
+SEQ_LEN = 80
 stringStop = "के का एक में की है यह और से हैं को पर इस होता कि जो कर मे गया करने किया लिये अपने ने बनी नहीं तो ही या एवं दिया हो इसका था द्वारा हुआ तक साथ करना वाले बाद लिए आप कुछ सकते किसी ये इसके सबसे इसमें थे दो होने वह वे करते बहुत कहा वर्ग कई करें होती अपनी उनके थी यदि हुई जा ना इसे कहते जब होते कोई हुए व न अभी जैसे सभी करता उनकी तरह उस आदि कुल एस रहा इसकी सकता रहे उनका इसी रखें अपना पे उसके \" \' [ ] . , ! : ; ?"
 QuestionsPre = ['का मोदी सरकार भारत की संवैधािनक संसाओ','पाथेर पाचाली फिल्म को विदेशी पुरस्कार से कब सम्मानित किया गया','इस बार कौन सी फिल्म हिट होगी ','राष्ट्रीय स्वयंसेवक संघ का मुख्यालय कहाँ है ?','टाइगर श्रॉफ की पहली फिल्म का क्या नाम है','फिल्म का क्या नाम है']
 
+
+# Acquiring and preprocessing the dataset
+train_df = preProcessor.read_csv('data/questions.csv')
+train_df = preProcessor.dfPreprocess(train_df)
+
+# Creating the embeddings
+print("Embedding will take about 4GB ram if system have less than 8GB RAM it will crash so exit using Ctrl+C ")
+print("Loading Embeddings Since size of embeddings is about 4GB, Loading will take time")
+emb = embeddings.create_embeddings()
+print("Embeddings Created")
+
+# Creating the stop words
 stopWords = preProcessor.StopWords(stringStop)
 
 choice = int(input("Press 1: Trained Model\nPress 2: Train a new Model : "))
 
 if(choice == 2):
+    #Creating the model
     model = modelObject.create_model()
     dg = preProcessor.batch_gen(train_df, batch_size, SEQ_LEN, stopWords, emb)
     model = modelObject.train(model, dg, train_df, batch_size, stopWords, emb, SEQ_LEN, preProcessor)
+    print("Model Trained ")
 
     choice2 = int(input("Press 1: Save Model\nPress 2: Skip saving : "))
     
@@ -35,6 +45,7 @@ elif(choice == 1):
     try:
         mPath = input("Enter the model Path")
         model = load_model(mPath)
+        print("Model Loaded")
     except:
         print("Model not found")
 
@@ -54,3 +65,5 @@ if(choice == 1):
     modelObject.predict(model,dg,Questions, stopWords,emb,SEQ_LEN,preProcessor)
 else:
     modelObject.predict(model,dg,QuestionsPre, stopWords,emb,SEQ_LEN,preProcessor)
+
+print("Program finished")
